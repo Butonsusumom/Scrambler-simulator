f8b48030e98fad77c40625dd12d7fc69e55f2c5a
Ksusha Tsubulko <43090851+Butonsusumom@users.noreply.github.com>
2020-06-05 16:15:12 +0300

    Add files via upload

---
+++ LFSR/LFSR/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace LFSR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(21, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(157, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(411, 44);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(121, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Real File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(530, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Encoded";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(21, 139);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(348, 419);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(412, 139);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(364, 419);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(368, 294);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "->";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(21, 576);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 47);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Chiper";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(157, 576);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 47);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Decipher";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 651);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ LFSR/LFSR/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace LFSR
+{
+   
+    public partial class Form1 : Form
+    {
+        public FileStream FileIn;
+        public FileStream FileEx;
+        public int by = 0;
+        public string s1;
+        public string s2;
+        public string s3;
+        public int outByte;
+        public long id =0;
+        public int counter = 0;
+        public Boolean flag = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string from10to2(int b)
+        {
+            string s="";
+            int ko=128;
+            for (int i = 0; i < 7; i++)
+            {
+                if ((b & ko) == 0) s += "0";
+                else s += "1";
+                ko = ko / 2;
+            }
+            s += " ";
+            return s;
+        }
+
+        public int from2to10(string s)
+        {
+            int n = 0;
+            int p = 1;
+            for(int i = 1; i <= s.Length; i++)
+            {
+                n += ((int)Char.GetNumericValue(s[s.Length - i])) * p;
+                p *= 2;
+            }
+            return n;
+        }
+
+        public int getNextByte()
+        {
+            by = FileIn.ReadByte();
+            if (by != -1)
+            {
+                
+                s2 += from10to2(by);
+                
+                if ((counter % 32767 == 0)&&(counter!=0))
+                {
+                    id++;
+                    flag = true;
+                }
+                counter++;
+            }
+            return by;
+        }
+
+        public void workWithByte(Int64 key)
+        {
+            long b = key >> 25;
+            b = b & 255;
+            int a = getNextByte();
+            if (a != -1)
+            {
+                s1 += from10to2((byte)b);
+                outByte = a ^ (int)b;
+                s3 += from10to2(outByte);
+                FileEx.WriteByte((byte)outByte);
+
+                if (flag)
+                {
+                    key = id * 32768;
+                    flag = false;
+                    s2 += "\n\n\n\n";
+                    s3 += "\n\n\n\n";
+
+                }
+            }
+        }
+
+        public void cipher(string s)
+        {
+            int f1, f2;
+            Int64 a = 1 << 2;
+            Int64 b = 1 << 24;
+            Int64 key = from2to10(s);
+            while (by != -1)
+            {
+                for(int i = 0; i < 8; i++)
+                {
+                    if ((a & key) == 0) f1 = 0; else f1 = 1;
+                    if ((b & key) == 0) f2 = 0; else f2 = 1;
+                    key = key << 1;
+                    key += f1 ^ f2;
+                }
+                workWithByte(key);
+            }
+
+        }
+
+
+            
+
+        private void button1_Click(object sender, EventArgs e) {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = openFile.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label3.Text = "->";
+            FileIn = new FileStream(richTextBox1.Text, FileMode.Open, FileAccess.Read);
+            FileEx = new FileStream(richTextBox1.Text.Replace(Path.GetExtension(richTextBox1.Text), "(ciphered)"+ Path.GetExtension(richTextBox1.Text)), FileMode.OpenOrCreate, FileAccess.Write);
+            s1 = "";
+            s2 = "";
+            s3 = "";
+            by = 0;
+            id = 0;
+            counter = 0;
+            flag = false;
+            cipher("0000000000000000000000001");
+            richTextBox2.Text =s2;
+            richTextBox3.Text =s3;
+           // richTextBox4.Text = s1;
+            FileIn.Close();
+            FileEx.Close();
+            }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label3.Text = "<-";
+            FileIn = new FileStream(richTextBox1.Text, FileMode.Open, FileAccess.Read);
+            FileEx = new FileStream(richTextBox1.Text.Replace(Path.GetExtension(richTextBox1.Text), "(deciphered)" + Path.GetExtension(richTextBox1.Text)), FileMode.OpenOrCreate, FileAccess.Write);
+            s1 = "";
+            s2 = "";
+            s3 = "";
+            by = 0;
+            id = 0;
+            counter = 0;
+            flag = false;
+            cipher("0000000000000000000000001");
+            richTextBox2.Text = s2;
+            richTextBox3.Text = s3;
+           // richTextBox4.Text = s1;
+            FileIn.Close();
+            FileEx.Close();
+        }
+    }
+}
